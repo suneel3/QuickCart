67413787b2e4799bfb3380b831f7bfb03ff20e8f
Suneel Kumar <shrikantsinghkashmeawahi@gmail.con>
2025-12-09 15:17:06 +0530

    fisrt-commit

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import { red } from "colors";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    _id:{type :String, required:true},
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    imageUrl:{ type: String,redquired:true  },
+    cartItems:{type:Object,default:{}}
+    },{minimize:false}
+);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
