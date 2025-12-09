a9e7d812a6f67daa52928f35367ff4bc2ed99262
Suneel Kumar <shrikantsinghkashmeawahi@gmail.con>
2025-12-09 15:36:05 +0530

    dbconnect

--- config/db.js
+++ config/db.js
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose
+let cached = global.mongoose;
 
-if(!cached){
-    cached = global.mongoose = {conn:null, promise:null};
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
-async function dbConnect(){
-    if(cached.conn){
-        return cached.conn;
-    }
-    if(!cached.promise){
-        const opts={
-            bufferCommands:false,
-        };
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then((mongoose)=>{
-            return mongoose;
-        })
-        
-    }
-    cached.conn = await cached.promise;
+
+async function dbConnect() {
+  if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose
+      .connect(`${process.env.MONGODB_URI}/quickcart`, opts)
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
-export default connectDB;
+export default dbConnect;
